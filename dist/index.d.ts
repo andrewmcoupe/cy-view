4345502cf19b756966eb2d85fafdd90f6506a6f6
Andy Coupe <andrewmcoupe@gmail.com>
2021-07-01 18:21:41 +0100

    Add pre publish script

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-interface DeviceList {
-    model: string;
-    width: number;
-    height: number;
-}
-declare type Device = object;
-declare type DeviceListArray = Device[];
-declare type Urls = string[];
-declare function prepareTestsForDevices(deviceList: DeviceListArray): (pageList: string[], callback: any) => void;
